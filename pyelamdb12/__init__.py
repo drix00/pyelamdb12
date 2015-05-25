4d3c45ce7384c731e6088b6b233107e57b33d639
Hendrix Demers <Hendrix Demers@localhost>
2015-05-25 19:26:44 -0400

    Add test data. Add project test suite.

---
+++ pyelamdb12/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""
+.. py:currentmodule:: __init__
+.. moduleauthor:: Hendrix Demers <hendrix.demers@mail.mcgill.ca>
+
+
+"""
+
+# Script information for the file.
+__author__ = "Hendrix Demers (hendrix.demers@mail.mcgill.ca)"
+__version__ = "0.1"
+__date__ = "Apr 6, 2015"
+__copyright__ = "Copyright (c) 2015 Hendrix Demers"
+__license__ = "GPL 3"
